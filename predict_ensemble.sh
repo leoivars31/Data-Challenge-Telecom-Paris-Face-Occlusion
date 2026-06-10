9443b8d901b6dfcceab781db4c2804582d78466c
leoivars31 <ivars.leo@gmail.com>
2026-06-10 19:42:07 +0200

    fix: temps max predict ensemble job

--- predict_ensemble.sh
+++ predict_ensemble.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=24G
-#SBATCH --time=1:00:00
+#SBATCH --time=24:00:00
 
 module purge
 module load python/3.11 cuda/12.4 miniconda3/25.5.1
